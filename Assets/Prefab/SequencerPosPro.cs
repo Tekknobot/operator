79b47c9a7091146c8ed045a1060799f1210d61d4
Tekknobot <thrillatron@hotmail.com>
2023-08-09 20:13:59 -0400

    Populate sequencer bars

--- Assets/Prefab/SequencerPosPro.cs
+++ Assets/Prefab/SequencerPosPro.cs
@@ -19,7 +19,7 @@ public class SequencerPosPro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject.Find("SQPOS "+ drumSequencer.GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
+        //GameObject.Find("SQPOS "+ drumSequencer.GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
         //GameObject.Find("SQPOS_SYNTH "+ synthSequencer.GetComponent<HelmSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
         GameObject.Find("SQPOS_SAMPLE "+ sampleSequencer.GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
 
@@ -38,17 +38,17 @@ public class SequencerPosPro : MonoBehaviour
 
         //////////
 
-        if (GameObject.Find("SynthSequencer_1") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 1) {
-            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_1").GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
+        if (GameObject.Find("DrumSampler_1") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 1) {
+            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_1").GetComponent<AudioHelm.SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
         }
-        if (GameObject.Find("SynthSequencer_2") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 2) {
-            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_2").GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
+        if (GameObject.Find("DrumSampler_2") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 2) {
+            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_2").GetComponent<AudioHelm.SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
         }
-        if (GameObject.Find("SynthSequencer_3") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 3) {
-            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_3").GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
+        if (GameObject.Find("DrumSampler_3") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 3) {
+            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_3").GetComponent<AudioHelm.SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
         }
-        if (GameObject.Find("SynthSequencer_4") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 4) {
-            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_4").GetComponent<SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
+        if (GameObject.Find("DrumSampler_4") && GameObject.Find("CurrentPattern_Drum").GetComponent<ShowCurrentPatternScript>().currentPatternDrum == 4) {
+            GameObject.Find("SQPOS "+ GameObject.Find("DrumSampler_4").GetComponent<AudioHelm.SampleSequencer>().currentIndex.ToString()).GetComponent<RawImage>().color = Color.yellow;
         }                             
     }
 }
--- Assets/UI/PopulateSeqBar.cs
+++ Assets/UI/PopulateSeqBar.cs
@@ -10,7 +10,9 @@ public class PopulateSeqBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PopulateBar(16);
+        PopulateSampleBar(16);
+        PopulateSynthBar(16);
     }
 
     public void PopulateBar(int numberToCycle) {
